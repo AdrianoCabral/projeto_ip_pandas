df1d8b1df8242303409bb693a8f2d86bed806f52
Adriano <afcf@cin.ufpe.br>
2020-02-29 20:38:16 -0300

    lendo todos os inputs

--- test.py
+++ test.py
@@ -55,7 +55,7 @@ for i in range(1,16):
     fMotor = False
     fTrans = False
     fMarca = False
-    line = 1
+    fMarca2 = False
     for linha in arquivo:
         
         #print(linha)
@@ -63,77 +63,76 @@ for i in range(1,16):
             print("yohoho")
         else:
 
-            if(fMarca == True):
+            if(fMarca2 == True):
                 marca = linha
-                fMarca = False    
-            if(re.findall('\AMarca', linha)):
-                fMarca = True
+                fMarca2 = False    
+            elif(re.findall('\AMarca', linha)):
+                fMarca2 = True
 
-            if(flano == True):
+            elif(flano == True):
                 linha = linha.replace(' ', '')
                 lista = linha.split('/')
-                ano = lista[1]
+                ano = int(lista[1])
                 flano = False
-            if(re.findall('\AANO', linha) or re.findall('\AAno', linha)):
+            elif(re.findall('\AANO', linha) or re.findall('\AAno', linha)):
                 flano = True
 
-            if(fkm == True):
+            elif(fkm == True):
                 kilometragem = int(linha)
                 fkm = False    
-            if(re.findall('\AKM', linha) or re.findall('\AQuilometragem', linha)):
+            elif(re.findall('\AKM', linha) or re.findall('\AQuilometragem', linha)):
                 fkm = True
                     
-            if(fCor == True):
+            elif(fCor == True):
                 cor = linha
                 fCor = False    
-            if(re.findall('\ACOR', linha) or re.findall('\ACor', linha)):
+            elif(re.findall('\ACOR', linha) or re.findall('\ACor', linha)):
                 fCor = True
             
-            if(fComb == True):
+            elif(fComb == True):
                 combustivel = linha
                 fComb = False    
-            if(re.findall('\ACombustível', linha)):
+            elif(re.findall('\ACombustível', linha)):
                 fComb = True
 
-            if(fMotor == True):
-                motor = linha
+            elif(fMotor == True):
+                motor = float(linha)
                 fMotor = False    
-            if(re.findall('\APotência do motor', linha)):
+            elif(re.findall('\APotência do motor', linha)):
                 fMotor = True            
 
-            if(fTrans == True):
+            elif(fTrans == True):
                 transmissão = linha
                 fTrans = False    
-            if(re.findall('\ACâmbio', linha)):
+            elif(re.findall('\ACâmbio', linha)):
                 fTrans = True
-
-            if(i < 6 and i > 12):
-                for word in linha.split():
-                    #print(word)
-                    if(fMarca == False):
-                        marca = word
-                        #print('olha a marca aqui: {}'.format(marca))
-                        fMarca = True
-                        
-                    else:
-                        
-                        if(fComb == True):
-                            combustivel = word
-                            fComb = False
-                        if(word == 'Combustível:'):
-                            fComb = True
-                        
-                        if(fMotor == True):
-                            motor = float(word)
-                            fMotor = False
-                        if(word == 'Motor:'):
-                            fMotor = True
-                        
-                        if(fTrans == True):
-                            transmissão = word
-                            fTrans = False
-                        if(word == 'Transmissão:'):
-                            fTrans = True
+            else:
+                    for word in linha.split():
+                        #print(word)
+                        if(fMarca == False):
+                            marca = word
+                            #print('olha a marca aqui: {}'.format(marca))
+                            fMarca = True
+                            
+                        else:
+                            
+                            if(fComb == True):
+                                combustivel = word
+                                fComb = False
+                            if(word == 'Combustível:'):
+                                fComb = True
+                            
+                            if(fMotor == True):
+                                motor = float(word)
+                                fMotor = False
+                            if(word == 'Motor:'):
+                                fMotor = True
+                            
+                            if(fTrans == True):
+                                transmissão = word
+                                fTrans = False
+                            if(word == 'Transmissão:'):
+                                fTrans = True
     #print(marca, ano, kilometragem, cor)
     c1 = Carro(marca, ano, kilometragem, cor, combustivel, motor, transmissão)
     #print(c1.getMarca(), c1.getAno(), c1.getKilometragem(), c1.getCor(), c1.getCombustivel(), c1.getMotor(), c1.getTransmissão())
@@ -148,7 +147,15 @@ for i in range(1,16):
     #print('\n')
 
 for c1 in lista_carros:
-    print(c1.getMarca(), c1.getAno(), c1.getKilometragem(), c1.getCor(), c1.getCombustivel(), c1.getMotor(), c1.getTransmissão())
+    #print(c1.getMarca(), c1.getAno(), c1.getKilometragem(), c1.getCor(), c1.getCombustivel(), c1.getMotor(), c1.getTransmissão())
+    print(c1.getMarca())
+    print(c1.getAno())
+    print(c1.getKilometragem())
+    print(c1.getCor())
+    print(c1.getCombustivel())
+    print(c1.getMotor())
+    print(c1.getTransmissão())
+
     print('\n')
 #dff.plot(kind='scatter',x='KM',y='Preco',color='blue')
 #plt.show()
